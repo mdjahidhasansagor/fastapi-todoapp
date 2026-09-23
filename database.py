5b3c394a00e410e6712b6f69903e6c55a7032fba
mdjahidhasansagor <jahid475312@gmail.com>
2026-09-23 19:53:44 +0600

    fix: sqlite check_same_thread added

--- database.py
+++ database.py
@@ -9,9 +9,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 # MYSQL SETUP LINK
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:12345678@127.0.0.1:3306/todoapplecation'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:12345678@127.0.0.1:3306/todoapplecation'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False})
 
 sessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
