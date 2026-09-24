110d706e0a4af86cbde9c6819dd1ef3dccd52b7e
mdjahidhasansagor <jahid475312@gmail.com>
2026-09-24 09:42:18 +0600

    postgresql connection added

--- database.py
+++ database.py
@@ -4,15 +4,18 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-# PostgreSQL SETUP Link
-# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:12345678@localhost/TodoApplecation'
+# PostgreSQL SETUP Link  jahid@2026J
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.xlsqshgxgovaycexhway:jahid@2026J@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
 
 # MYSQL SETUP LINK
 # SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:12345678@127.0.0.1:3306/todoapplecation'
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_same_thread": False})
+
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, 
+#     connect_args={"check_same_thread": False})
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 sessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
